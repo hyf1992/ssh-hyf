137b8114c98683020ab42b4a702a75969a83d854
hyf <1819078999@qq.com>
2017-03-27 14:35:24 +0800

    基本配置

--- src/main/java/com/whitehorse/qingzhi/controller/UserController.java
+++ src/main/java/com/whitehorse/qingzhi/controller/UserController.java
@@ -1,8 +1,28 @@
 package com.whitehorse.qingzhi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller(value="user")
+import com.whitehorse.qingzhi.entity.User;
+import com.whitehorse.qingzhi.service.UserService;
+
+@Controller
+@RequestMapping(value="user")
 public class UserController {
-
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/select")
+	public @ResponseBody User selectById(int id,Model model){
+		User user = userService.select(id);
+		System.out.println(id);
+		System.out.println(user);
+		model.addAttribute(user);
+		return user;
+	}
 }
