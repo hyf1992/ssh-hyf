c127cd8b47451cb480ffcafd4b0b9e8ce5436689
hyf <1819078999@qq.com>
2017-03-28 11:54:00 +0800

    模板

--- src/main/java/com/whitehorse/qingzhi/dao/BaseDao.java
+++ src/main/java/com/whitehorse/qingzhi/dao/BaseDao.java
@@ -4,6 +4,7 @@ package com.whitehorse.qingzhi.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.whitehorse.qingzhi.util.Util;
@@ -13,18 +14,22 @@ public class BaseDao<T> {
 	@Autowired
 	private SessionFactory sessionFactory;
 	
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
 	/**
 	 * 获取当前session
 	 * @return
 	 */
 	private Session getCurrentSession() {
+		
 		return sessionFactory.getCurrentSession();
 	} 
 	
 	@SuppressWarnings("unchecked")
 	public T select(int id){
 		//getCurrentSession().createQuery("from User u where u.userId="+id).uniqueResult();
-		return (T) getCurrentSession().get(Util.getSuperClassGenricType(this.getClass(), 0), id);
+		//return (T) getCurrentSession().get(Util.getSuperClassGenricType(this.getClass(), 0), id);
+		return (T) hibernateTemplate.get(Util.getSuperClassGenricType(this.getClass(), 0), id);
 	}
 	
 }
